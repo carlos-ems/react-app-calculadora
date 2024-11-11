cecaad75cfa6d94e3134de7837f76b7180eafbd9
carlos <cmsilva1601@gmail.com>
2024-11-11 19:49:33 -0300

    Implementação da função soma

--- src/App.js
+++ src/App.js
@@ -1,11 +1,80 @@
 import Input from './components/input';
-import { Container, Content } from './styles';
+import Button from './components/button';
+import { Container, Content, Row } from './styles';
+import { useState } from 'react';
 
 const App= () => {
+  const [currentNumber, setCurrentNumber] = useState('0');
+  const [firstNumber, setFirstNumber] = useState('0');
+  const [operation, setOperation] = useState('');
+
+  const handlerOnClear = () => {
+    setCurrentNumber('0')
+    setFirstNumber('0')
+    setOperation('')
+  }
+
+  const handleAddNumber = (number) => {
+    setCurrentNumber(prev => `${prev === '0' ? '' : prev}${number}`)
+  }
+
+  const handleSumNumbers = () => {
+    if(firstNumber === '0'){
+      setFirstNumber(String(currentNumber))
+      setCurrentNumber('0')
+      setOperation('+')
+    }else{
+      const sum = Number(firstNumber) + Number(currentNumber)
+      setCurrentNumber(String(sum))
+      setOperation('')
+    }
+  }
+
+  const handleEqualsTo = () => {
+    if(!firstNumber === '0' && operation !== '' && currentNumber !== '0'){
+      switch(operation){
+        case '+':
+          handleSumNumbers();
+          break;
+          default:
+          break;
+      }
+    }
+  }
+
   return (
     <Container>
       <Content>
-        <Input/>
+        <Input value={currentNumber}/>
+        <Row>
+          <Button label="C" onClick={handlerOnClear}/>
+          <Button label="+/-"/>
+          <Button label="%"/>
+          <Button label="/"/>
+        </Row>
+        <Row>
+          <Button label="7" onClick={() => handleAddNumber('7')}/>
+          <Button label="8" onClick={() => handleAddNumber('8')}/>
+          <Button label="9" onClick={() => handleAddNumber('9')}/>
+          <Button label="x"/>
+        </Row>
+        <Row>
+          <Button label="4" onClick={() => handleAddNumber('4')}/>
+          <Button label="5" onClick={() => handleAddNumber('5')}/>
+          <Button label="6" onClick={() => handleAddNumber('6')}/>
+          <Button label="-"/>
+        </Row>
+        <Row>
+          <Button label="1" onClick={() => handleAddNumber('1')}/>
+          <Button label="2" onClick={() => handleAddNumber('2')}/>
+          <Button label="3" onClick={() => handleAddNumber('3')}/>
+          <Button label="+" onClick={handleSumNumbers}/>
+        </Row>
+        <Row>
+          <Button label="0"onClick={() => handleAddNumber('0')}/>
+          <Button label=","onClick={() => handleAddNumber('')}/>
+          <Button label="=" onClick={handleEqualsTo}/>
+        </Row>
       </Content>
     </Container>
   );
--- src/components/input/styles.js
+++ src/components/input/styles.js
@@ -12,12 +12,17 @@ export const InputContainer = styled.div`
     font-size: 24px;
     font-family: 'Roboto';
 
-    input{
+    input {
         width: 100%;
         height: 75px;
-        background-color: #AAFFAA;
+        background-color: #000000;
         border: 0;
-        float: right;
-        clear: both;
+        display: flex;
+        flex-direction: column;
+        align-items: flex-end;
+        padding: 0 10px;
+        font-size: 24px;
+        font-family: 'Roboto';
+        color: #FFFFFF;
     }
 `
